eea9f4fa3051a30f6979ee0e45e3cb335e3b1161
Nicholas Petree <123441251+Pete1230z@users.noreply.github.com>
2023-08-22 21:58:58 -0700

    When a cell is clicked on an X appears

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
-const player1 = 'x';
-const player2 = 'circle';
+const playerClassX = 'x';
+const playerClassCircle = 'circle';
+const winningCombinations = [
+	[0,1,2],
+	[3,4,5],
+	[6,7,8],
+	[0,3,6],
+	[1,4,7],
+	[2,5,8],
+	[0,4,8],
+	[2,4,6]
+]
+const cellElements = document.querySelectorAll('[data-cell]');
+
+cellElements.forEach(cell => {
+	cell.addEventListener('click', playerMove)
+});
+
+function playerMove(e) {
+	const cell = e.target;
+	cell.className = 'cell x';
+	console.log(cell)
+}
+//StartGame
+//placeMark
+//Check for win
+//Check for draw
+//Switch turns
+
+
+
 
